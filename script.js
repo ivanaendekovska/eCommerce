d5b98d40968e1601da126598ba411954d110d423
ivanaendekovska <ivana.endekovska@gmail.com>
2024-05-11 15:58:05 +0200

    Initial submit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var getyoursDiv = document.getElementById("getyours");
+  
+    getyoursDiv.addEventListener("click", function(e) {
+      getyoursDiv.classList.add("active");
+      
+    });
+  });
